17e949a2c6c28bedc64565eb36fa5745a10beee8
unknown <govindgnair23@gmail.com>
2025-03-09 09:07:46 -0400

    Added evaluations for first agentic workflow

--- evals/eval_workflow1.py
+++ evals/eval_workflow1.py
@@ -64,72 +64,120 @@ sars = [
                         
             '3489728': "Nine wire transfers totaling $225,000 were sent from John Doe's personal account #12345-6789 at Dummy_Bank_1 to Kulkutta Building Supply Company, account #3489728 at the Bank of Anan in Dubai, UAE, between January 17, 2003, and March 21, 2003."}
     ),
-    # Add second SAR
-    # SAR(
-    #     sar_name="sar_train2",
-    #     sar_narrative = sar_narratives[1],
-    #     gold_entities={
-    #        "Individuals": ["John Doe", "Jane Doe"],
-    #         "Organizations": ["Doe’s Auto Sales"],
-    #         "Financial Institutions": ["XYZ Bank"]
-    #     },
-    #     gold_account_ids= ["1234567", "Dummy_Acct_1"],
-    #     gold_acct_to_fi={
-    #                 "1234567": "Dummy_Bank_1" ,
-    #                 "Dummy_Acct_1": "XYZ Bank"
-    #             },
-    #     gold_acct_to_cust= {
-    #             "1234567": "Doe’s Auto Sales",
-    #             "Dummy_Acct_1": "Doe’s Auto Sales"
-    #                    }
-    # ),
-    # # Add third SAR
-    # SAR(
-    #     sar_name="sar_train3",
-    #     sar_narrative = sar_narratives[2],
-    #     gold_entities={
-    #                 "Individuals": ["John Doe", "Jennifer Doe"],
-    #                 "Organizations": ["Quickie Car Wash"],
-    #                 "Financial Institutions": ["Aussie Bank"]
-    #             },
-    #     gold_account_ids=  ["12345678910", "981012345"],
-    #     gold_acct_to_fi={
-    #                 "12345678910": "Dummy_Bank_1" ,
-    #                 "981012345": "Aussie Bank"
-    #             },
-    #         gold_acct_to_cust=  {
-    #                     "12345678910": "John Doe",
-    #                     "981012345": "Jennifer Doe"
-    #                 }
-    # ),
-    # # Add fourth SAR
-    # SAR(
-    #     sar_name="sar_train4",
-    #     sar_narrative = sar_narratives[3],
-    #     gold_entities={
-    #                     "Individuals": ["Paul Lafonte"],
-    #                     "Organizations": ["Sky Corporation", "Sea Corporation", "Tolinka Inc."],
-    #                     "Financial Institutions": ["Bank of Mainland", "Bank XYZ", "Bank of Poland", "Artsy Bank"]
-    #                 },
-    #     gold_account_ids=   ["54321098", "12345678", "689472", "456781234", "Dummy_Acct_1","Dummy_Acct_2"],
-    #     gold_acct_to_fi={
-    #                 "54321098": "Bank of Mainland",
-    #                 "12345678": "Bank of Mainland",
-    #                 "689472": "Bank XYZ",
-    #                 "456781234": "Artsy Bank",
-    #                 "Dummy_Acct_1": "Bank of Poland",
-    #                  "Dummy_Acct_2": "Bank of Mainland"
-    #             },
-    #     gold_acct_to_cust=  {
-    #                     "54321098": "Sky Corporation",
-    #                     "12345678": "Sea Corporation",
-    #                     "689472": "Tolinka Inc.",
-    #                     "456781234": "Paul Lafonte",
-    #                     "Dummy_Acct_1": "Bank XYZ",
-    #                     "Dummy_Acct_2": "Bank of Poland"
-    #                 }
-    # ),
 
+    ## Add Second SAR
+    SAR(
+        sar_name="sar_train2",
+        sar_narrative = sar_narratives[1],
+        gold_entities={
+              "Individuals": ["John Doe","Jane Doe"],
+              "Organizations": ["Doe's Auto Sales"],
+              "Financial_Institutions": ["XYZ Bank"]
+        },
+        gold_account_ids=["1234567"],
+        gold_acct_to_fi={
+            "1234567": "Dummy_Bank_1"
+        },
+        gold_acct_to_cust={
+           "1234567": "Doe's Auto Sales"
+        },
+        gold_fi_to_acct_to_cust = { "Dummy_Bank_1": {"1234567": "CUST_001"},
+        "XYZ Bank": {}
+                 },
+
+        gold_narrative =  {
+             "1234567": "The account #1234567 for Doe\u2019s Auto Sales shows unusual activity characterized by structured cash deposits. On six occasions in June 2003, cash deposits of $9,800 were made, possibly to avoid the filing of a currency transaction report. Deposits were made by John Doe on 06/03, 06/09, and 06/11 at the Happy Valley branch, while Jane Doe made deposits on 06/04, 06/10, and 06/12 at the Main Office branch. Following these deposits, checks for $9,800 were issued and subsequently deposited at XYZ Bank on 06/04, 06/05, 06/10, 06/11, 06/12, and 06/13. The source of the cash is unknown, and this pattern appears to evade the reporting requirements of the Bank Secrecy Act."}
+    ),
+
+    ## Add third SAR
+    SAR(
+        sar_name="sar_train3",
+        sar_narrative = sar_narratives[2],
+        gold_entities={
+              "Individuals": ["John Doe","Jane Doe"],
+              "Organizations": ["Quickie Car Wash"],
+              "Financial_Institutions": ["Aussie Bank"]
+        },
+        gold_account_ids=["12345678910","981012345"],
+        gold_acct_to_fi={
+            "12345678910": "Dummy_Bank_1",
+            "981012345": "Aussie Bank"
+        },
+        gold_acct_to_cust={
+           "12345678910": "John Doe",
+           "981012345": "Jennifer Doe"
+        },
+        gold_fi_to_acct_to_cust = {
+        "Dummy_Bank_1": {
+            "12345678910": "CUST_001"
+        },
+        "Aussie Bank": {
+            "981012345": "CUST_002"
+        }
+            },
+
+        gold_narrative =  {
+                 "12345678910": "John Doe made structured cash deposits into his personal checking account at Dummy_Bank_1, totaling $29,650, with the following details: 03/15/02 - $9,950.00; 03/17/02 - $9,700.00; 03/18/02 - $10,000. Additionally, he conducted immediate wire transfers to Jennifer Doe's account at Aussie Bank, totaling $29,500. The wire transfer details are as follows: 03/16/02 - $9,900.00; 03/18/02 - $9,700.00; 03/19/02 - $9,900.00.",
+                 "981012345": "Jennifer Doe receives wire transfers from John Doe's account at Dummy_Bank_1, with total amounts received being $29,500 over several transactions: 03/16/02 - $9,900.00; 03/18/02 - $9,700.00; 03/19/02 - $9,900.00."}
+    ),
+
+    # Add Fourth SAR
+
+    SAR(
+        sar_name="sar_train4",
+        sar_narrative = sar_narratives[2],
+        gold_entities={
+                "Individuals": ["Paul Lafonte"],
+        "Organizations": [
+            "Sky Corporation",
+            "Sea Corporation",
+            "Tolinka Inc."
+        ],
+        "Financial_Institutions": [
+            "Bank of Mainland",
+            "Bank XYZ",
+            "Bank of Poland",
+            "Artsy Bank"
+        ]
+        },
+        gold_account_ids=[  "54321098","12345678","689472","Dummy_Acct_1"],
+        gold_acct_to_fi={
+            "54321098": "Bank of Mainland",
+            "12345678": "Bank of Mainland",
+            "689472": "Bank XYZ",
+            "Dummy_Acct_1": "Dummy_Bank_1"
+        },
+        gold_acct_to_cust={
+           "54321098": "Sky Corporation",
+            "12345678": "Sea Corporation",
+           "689472": "Tolinka Inc.",
+           "Dummy_Acct_1": "Dummy_Customer_1"
+        },
+        gold_fi_to_acct_to_cust = {
+        "Bank of Mainland": {
+            "54321098": "Sky Corporation",
+            "12345678": "Sea Corporation"
+        },
+        "Bank XYZ": {
+            "689472": "Tolinka Inc."
+        },
+        "Dummy_Bank_1": {
+            "Dummy_Acct_1": "Dummy_Customer_1"
+        }
+            },
+
+        gold_narrative =  {
+                 {
+        "54321098": "Sky Corporation had 284 cash deposits totaling $2,710,000 at Bank of Mainland, conducted through three main branches: North Burlington, South Burlington, and West Burlington. The average deposit amount ranged from $8,720 to $16,500. The day after deposits, 274 outgoing wire transfers totaling $2,697,000 were conducted, typically sent to Paul Lafonte at Artsy Bank, account #456781234 in Paris, France, using a remote computer terminal.The activity occured for the period  2/2/99 through 6/20/01",
+
+        "12345678": "Sea Corporation had 200 cash deposits totaling $1,900,000 at Bank of Mainland, with many transactions conducted on the same day at multiple branches to potentially circumvent federal reporting requirements. The company processed 198 outgoing wire transfers totaling $1,866,000, usually mirroring the deposits from the previous day, and also directed to Paul Lafonte at Artsy Bank, account #456781234 in Paris, France.The activity occured for the period  2/2/99 through 6/20/01",
+
+        "689472": "Tolinka Inc. is a customer of Bank XYZ and received 15 incoming wire transfers affecting account #12345678 at Bank of Mainland. Bank XYZ was unable to provide documentation for Tolinka Inc., and after inquiries, Tolinka Inc. closed its account without explanation.The activity occured for the period  2/2/99 through 6/20/01"
+    }}
+    )
+
+
+    
 ]
 
 if __name__ == "__main__":
